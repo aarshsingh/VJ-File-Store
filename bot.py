fd4cfb61ba96b966456a6235ec9b711fb4632c94
aarshsingh <156768705+aarshsingh@users.noreply.github.com>
2024-08-26 17:29:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+import pyrogram.utils
 
+pyrogram.utils.MIN_CHAT_ID = -999999999999
+pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
 import sys
 import glob
